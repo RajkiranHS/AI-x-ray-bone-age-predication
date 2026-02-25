1340aef8f472b6f2359e73857a8867adf28c25db
Rajkiran Suryawanshi <45843450+RajkiranHS@users.noreply.github.com>
2026-02-25 09:27:39 +0000

    AI

--- AI-model/run_AI_bone_model.py
+++ AI-model/run_AI_bone_model.py
@@ -9,6 +9,7 @@ img = cv2.resize(img, (224, 224))
 img = img / 255.0  # Normalize
 img = np.reshape(img, (1, 224, 224, 1))
 
+
 predicted_age = model.predict(img)
 print(f"Predicted bone age: {predicted_age[0][0]:.2f} months")
 print("Predicted bone age in years:", predicted_age/12)
